ba3dd2e041abe401bd8dd32eab80e13af6e35e49
shafik720 <shafikrasel5@gmail.com>
2022-11-18 22:37:46 +0600

    setting up header

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <h2>This is header</h2>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/contact'>Contact Us</Link>
+                <Link to='/Login'>Login</Link>                
+            </nav>
         </div>
     );
 };
