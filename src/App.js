c7a1c689f00a0a54cbdf62e0fcacf7f6cfa491d9
shafik720 <shafikrasel5@gmail.com>
2022-11-18 22:35:59 +0600

    working on route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './Pages/Login/Login';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World</h2>
+      <Routes>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+      </Routes>
     </div>
   );
 }
