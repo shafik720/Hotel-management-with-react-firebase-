9d7fbf2ca1a95989303d5789fbd674a573db1ffe
shafik720 <shafikrasel5@gmail.com>
2022-11-18 22:28:22 +0600

    working on route

---
+++ src/Pages/Error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className='text-center'>
+            <h2 style={{textAlign:'center'}}>Error 404 Not Found</h2>
+        </div>
+    );
+};
+
+export default Error;
