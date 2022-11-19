fed33bd2271b344310552d74077030eeea99c396
shafik720 <shafikrasel5@gmail.com>
2022-11-19 13:41:54 +0600

    working on cities

--- src/Pages/Home/Cities/Cities.js
+++ src/Pages/Home/Cities/Cities.js
@@ -12,6 +12,24 @@ const Cities = () => {
                     </div>
                     <h2>Cox's Bazar</h2>
                 </div>
+                <div draggable className="city">
+                    <div className="city-img">
+                        <img src={cox} alt="" />
+                    </div>
+                    <h2>Cox's Bazar</h2>
+                </div>
+                <div draggable className="city">
+                    <div className="city-img">
+                        <img src={cox} alt="" />
+                    </div>
+                    <h2>Cox's Bazar</h2>
+                </div>
+                <div draggable className="city">
+                    <div className="city-img">
+                        <img src={cox} alt="" />
+                    </div>
+                    <h2>Cox's Bazar</h2>
+                </div>
             </div>
         </div>
     );
