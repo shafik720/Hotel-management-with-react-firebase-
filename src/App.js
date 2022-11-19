2c7efc59f0bc3e67ef10bee548d3426e21a05da7
shafik720 <shafikrasel5@gmail.com>
2022-11-19 11:52:19 +0600

    working on homepage

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import About from './Pages/About/About';
 import Contact from './Pages/Contact/Contact';
 import Error from './Pages/Error/Error';
 import Header from './Pages/Header/Header';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/contact' element={<Contact></Contact>}></Route>
---
+++ src/Pages/Home/Cities/Cities.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cities = () => {
+    return (
+        <div>
+            <h2>Select your City</h2>
+        </div>
+    );
+};
+
+export default Cities;
