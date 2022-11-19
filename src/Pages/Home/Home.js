0bf25097be194f903323e87e773769fa3c9b7c92
shafik720 <shafikrasel5@gmail.com>
2022-11-19 11:53:54 +0600

    working on homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Cities from './Cities/Cities';
 
 const Home = () => {
     return (
         <div className='container'>
             <h2>Home Page</h2>
+            <Cities></Cities>
         </div>
     );
 };
