eacc7f78f4dea941149e3157db18850a65e99c94
shafik720 <shafikrasel5@gmail.com>
2022-11-20 11:35:55 +0600

    adding hotel data t

---
+++ src/Pages/Home/Cities/HotelDisplay/HotelDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HotelDisplay.css'
+
+const HotelDisplay = () => {
+    return (
+        <div>
+            <h2>Hotel Name : </h2>
+        </div>
+    );
+};
+
+export default HotelDisplay;
