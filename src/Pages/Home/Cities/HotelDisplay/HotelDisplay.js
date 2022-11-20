75f85bd05d43195d2f0a53b15e5df299dae12b69
shafik720 <shafikrasel5@gmail.com>
2022-11-20 11:59:02 +0600

    adding hotel data

--- src/Pages/Home/Cities/HotelDisplay/HotelDisplay.js
+++ src/Pages/Home/Cities/HotelDisplay/HotelDisplay.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './HotelDisplay.css'
 
-const HotelDisplay = () => {
+const HotelDisplay = (props) => {
+    let[thumbImg, swimmingPool, stars, restaurant, ratings, price, hotelName, freeWifi, freeParking, fitnessCenter, city, beachFront, bar, airportShuttle ] = props.index;
     return (
-        <div>
-            <h2>Hotel Name : </h2>
+        <div className=''>
+            <h2>Hotel Name : {props.index.hotelName} </h2>
         </div>
     );
 };
