72e6fabec6474c1fed59ec7220ade7ead55b17cc
shafik720 <shafikrasel5@gmail.com>
2022-11-20 11:19:26 +0600

    started adding hotel data in js object

---
+++ src/fake data/all-hotels.js
@@ -0,0 +1,18 @@
+
+let allHotels = [
+    {
+        id : 1,
+        city : 'coxbazar',
+        hotelName : 'Hotel Sea Crown',
+        ratings : 7.2,
+        price : 300,
+        beds : ['Single Bed','Double Bed','Family Suit','Vip'],
+        freeWifi : 'yes',
+        freeParking : 'yes',
+        airportShuttle : 'yes',
+        restaurant : 'yes',
+        bar : 'yes',
+        swimmingPool : 'no',
+        beachFront : 'yes'
+    }
+]
