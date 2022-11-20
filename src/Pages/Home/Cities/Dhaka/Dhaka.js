d9cb79c0308b38c679f0980941f2ff23bc24f6ca
shafik720 <shafikrasel5@gmail.com>
2022-11-20 13:49:53 +0600

    working on dhaka hotels

--- src/Pages/Home/Cities/Dhaka/Dhaka.js
+++ src/Pages/Home/Cities/Dhaka/Dhaka.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import HotelDisplay from '../HotelDisplay/HotelDisplay';
 
 const Dhaka = () => {
+    let[hotels, setHotels] = useState([]);
+    useEffect(()=>{
+        fetch('all-hotels.json')
+        .then(res=>res.json())
+        .then(data=>setHotels(data))
+    },[])
     return (
         <div>
-            <h2>Dhaka</h2>
+            <h2 className='text-center mt-5'>Hotels In Dhaka</h2>
+            <div className="hotels-div container my-5">
+                <div className="row gy-4">
+                {
+                    hotels.map(index=> index.city === 'dhaka' && <HotelDisplay 
+                    key = {index.id}
+                    index={index}
+                ></HotelDisplay>)
+                }                    
+                </div>                
+            </div>
         </div>
     );
 };
