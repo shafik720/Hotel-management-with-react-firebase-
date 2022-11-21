3bdcdaf2c7618a4d40fa2b7b0b2839f9b4f6b927
shafik720 <shafikrasel5@gmail.com>
2022-11-21 11:54:05 +0600

    working on single hotel page

--- src/Pages/SingleHotelDetails/SingleHotelDetail/SingleHotelDetail.js
+++ src/Pages/SingleHotelDetails/SingleHotelDetail/SingleHotelDetail.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const SingleHotelDetail = (props) => {
+    let { id, thumbImg, swimmingPool, stars, restaurant, ratings, price, hotelName, freeWifi, freeParking, fitnessCenter, city, beachFront, bar, airportShuttle } = props.index;
     return (
         <div>
-            <h2>Hotel Name : {props.index.hotelName} </h2>
+            <h2>Hotel Name : {hotelName} </h2>
         </div>
     );
 };
