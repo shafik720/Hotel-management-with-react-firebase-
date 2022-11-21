523da0577d737854e0d1da41d3843a4f1a6a87da
shafik720 <shafikrasel5@gmail.com>
2022-11-21 12:57:30 +0600

    before making table for room type

--- src/Pages/SingleHotelDetails/SingleHotelDetail/SingleHotelDetail.js
+++ src/Pages/SingleHotelDetails/SingleHotelDetail/SingleHotelDetail.js
@@ -9,18 +9,19 @@ const SingleHotelDetail = (props) => {
     return (
         <div className='container'>
             <div className="row">
-                <div className="col-lg-8 mx-auto my-5 border">
-                    <div className="">
-                        <h2 className='text-center mb-5'>Hotel Name : {hotelName} </h2>
-                        <div className="row singleHotel-desc">
-                            <div className="col-lg-6">
-                                <div className="">
+                <div className="col-lg-8 mx-auto my-5 ">
+                    <div className="single-hotel-div">
+                        <h2 className='text-center mb-5'> {hotelName} </h2>
+                        <div className="row singleHotel-desc ">
+                            <div className="col-lg-6 ">
+                                <div className=" ">
                                     <div className="singleHotel-desc-left">
                                         {/* <p>Ratings : <span className='darkRed-text'>{ratings}</span> </p>
                                         <p>Price : <span className='darkRed-text'>{price}</span>  </p> */}
                                         <p> <span className='iconBox'><FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon></span> Fitness Center : {fitnessCenter} </p>
                                         <p><span className='iconBox'><FontAwesomeIcon icon={faUmbrellaBeach}></FontAwesomeIcon></span>Beach Front : {beachFront} </p>
                                         <p><span className='iconBox'><FontAwesomeIcon icon={faUtensils}></FontAwesomeIcon></span> Restaurant : {restaurant} </p>
+                                        <p><span className='iconBox'><FontAwesomeIcon icon={faCar}></FontAwesomeIcon></span>Parking : {freeParking} </p>
                                     </div>
                                 </div>
                             </div>
@@ -29,13 +30,13 @@ const SingleHotelDetail = (props) => {
                                     <div className="singleHotel-desc-left">
                                         <p><span className='iconBox'><FontAwesomeIcon icon={faPersonSwimming}></FontAwesomeIcon></span>Swimming Pool : {swimmingPool} </p>
                                         <p><span className='iconBox'><FontAwesomeIcon icon={faWifi}></FontAwesomeIcon></span>Free Wifi : {freeWifi} </p>
-                                        <p><span className='iconBox'><FontAwesomeIcon icon={faCar}></FontAwesomeIcon></span>Parking : {freeParking} </p>
                                         <p><span className='iconBox'><FontAwesomeIcon icon={faChampagneGlasses}></FontAwesomeIcon></span>Bar : {bar} </p>
                                         <p><span className='iconBox'><FontAwesomeIcon icon={faPlaneDeparture}></FontAwesomeIcon></span>Airport Shuttle : {airportShuttle} </p>
                                     </div>
                                 </div>
                             </div>
                         </div>
+
                         
                     </div>
                 </div>
