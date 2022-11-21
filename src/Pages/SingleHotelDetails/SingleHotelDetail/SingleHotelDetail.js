29c75e1c6424d4a055ddc60db2a59c8b860ae489
shafik720 <shafikrasel5@gmail.com>
2022-11-21 11:56:05 +0600

    working on single hotel page

--- src/Pages/SingleHotelDetails/SingleHotelDetail/SingleHotelDetail.js
+++ src/Pages/SingleHotelDetails/SingleHotelDetail/SingleHotelDetail.js
@@ -3,8 +3,14 @@ import React from 'react';
 const SingleHotelDetail = (props) => {
     let { id, thumbImg, swimmingPool, stars, restaurant, ratings, price, hotelName, freeWifi, freeParking, fitnessCenter, city, beachFront, bar, airportShuttle } = props.index;
     return (
-        <div>
-            <h2>Hotel Name : {hotelName} </h2>
+        <div className='container'>
+            <div className="row">
+                <div className="col-lg-8 mx-auto">
+                    <div className="text-center">
+                        <h2>Hotel Name : {hotelName} </h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
