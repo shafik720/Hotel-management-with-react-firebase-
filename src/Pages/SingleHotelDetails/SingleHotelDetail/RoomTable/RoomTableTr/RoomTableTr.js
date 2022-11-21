fd96b5fee6786737564d586e956ce957aec96035
shafik720 <shafikrasel5@gmail.com>
2022-11-21 17:11:06 +0600

    working on building the  hotel database

--- src/Pages/SingleHotelDetails/SingleHotelDetail/RoomTable/RoomTableTr/RoomTableTr.js
+++ src/Pages/SingleHotelDetails/SingleHotelDetail/RoomTable/RoomTableTr/RoomTableTr.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faBaby } from '@fortawesome/free-solid-svg-icons';
+
 
 const RoomTableTr = (props) => {
+    let superiorTwin = <span className='bedIcon'><FontAwesomeIcon icon={faUser} /> <FontAwesomeIcon icon={faUser} /> + <FontAwesomeIcon icon={faBaby} /></span> ;
     let td = props.index;
     if(td == 3){
-        td = 'Triple bed';
+        td = superiorTwin;
     }
     return (
         <td>
-            {td}            
+            {td}                        
         </td>
     );
 };
