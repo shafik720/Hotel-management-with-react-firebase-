1267a3f8d2da40e5f10637dd805bb15d2adbdd12
shafik720 <shafikrasel5@gmail.com>
2022-11-21 12:22:30 +0600

    working on single hotel page

--- src/Pages/SingleHotelDetails/SingleHotelDetail/SingleHotelDetail.js
+++ src/Pages/SingleHotelDetails/SingleHotelDetail/SingleHotelDetail.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import './SingleHotelDetail.css'
+import './SingleHotelDetail.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell, faUmbrellaBeach, faUtensils } from '@fortawesome/free-solid-svg-icons';
+
 
 const SingleHotelDetail = (props) => {
     let { id, thumbImg, swimmingPool, stars, restaurant, ratings, price, hotelName, freeWifi, freeParking, fitnessCenter, city, beachFront, bar, airportShuttle } = props.index;
@@ -9,15 +12,15 @@ const SingleHotelDetail = (props) => {
                 <div className="col-lg-8 mx-auto my-5 border">
                     <div className="">
                         <h2 className='text-center mb-5'>Hotel Name : {hotelName} </h2>
-                        <div className="row">
+                        <div className="row singleHotel-desc">
                             <div className="col-lg-6">
                                 <div className="">
                                     <div className="singleHotel-desc-left">
                                         <p>Ratings : <span className='darkRed-text'>{ratings}</span> </p>
                                         <p>Price : <span className='darkRed-text'>{price}</span>  </p>
-                                        <p>Fitness Center : {fitnessCenter} </p>
-                                        <p>Beach Front : {beachFront} </p>
-                                        <p>Restaurant : {restaurant} </p>
+                                        <p> <span><FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon></span> Fitness Center : {fitnessCenter} </p>
+                                        <p><span><FontAwesomeIcon icon={faUmbrellaBeach}></FontAwesomeIcon></span>Beach Front : {beachFront} </p>
+                                        <p><span><FontAwesomeIcon icon={faUtensils}></FontAwesomeIcon></span> Restaurant : {restaurant} </p>
                                     </div>
                                 </div>
                             </div>
