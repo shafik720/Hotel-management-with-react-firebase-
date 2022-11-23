6e996b094c99541ee245cd77410402ee9ed8c79e
shafik720 <shafikrasel5@gmail.com>
2022-11-23 22:06:24 +0600

    working on firebase

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Signup.css'
 
 const Signup = () => {
     return (
         <div>
-            
+            <div className="container my-3">
+                <div className="row">
+                    <div className="col-lg-6 mx-auto">
+                        <div className=" form-div ">
+                            <h2>Log In</h2>
+                            <hr style={{ marginBottom: "40px" }} />
+                            <form action="" >
+                                <div className="email-field">
+                                    <p>Email :</p>
+                                    <input  type="email" name="" id="" />
+                                </div>
+                                <div className="password-field">
+                                    <p>Password :</p>
+                                    <input  type="password" name="" id="" />
+                                </div>
+                                <div className="text-center error-area">
+                                    
+                                </div>
+                                <button>Login</button>
+                                <p className="signUpText">Dont Have an Account ? <Link to="/signup">Sign Up Here</Link> </p>
+                                <h4>Or</h4>
+                                <div draggable className="googleButton">
+                                    <img  alt="" />
+                                    <h4>Sign in Using Google</h4>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
