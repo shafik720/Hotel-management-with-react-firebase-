39a4859e3105a1d52624bbf44c11001751c66993
shafik720 <shafikrasel5@gmail.com>
2022-11-23 20:05:42 +0600

    working on styles

--- src/Pages/SingleHotelDetails/SingleHotelDetail/SingleHotel/SingleHotel.js
+++ src/Pages/SingleHotelDetails/SingleHotelDetail/SingleHotel/SingleHotel.js
@@ -4,17 +4,17 @@ import Hotel from './Hotel/Hotel';
 import './SingleHotel.css';
 
 const SingleHotel = (props) => {
-    function handleBookButton(element){
+    function handleBookButton(element) {
         addToDb(element);
     }
     return (
-        <div className='row table-parent'>            
+        <div className='row table-parent'>
             <div className="col-lg-12 table fw-bold fw-bolder ">
-            {
-                props.index.map(index=><Hotel index={index}></Hotel>)
-            }    
-            <button onClick={()=>handleBookButton(props.index)}>Book Now</button>
-            </div>            
+                {
+                    props.index.map(index => <Hotel index={index}></Hotel>)
+                }
+                <button onClick={() => handleBookButton(props.index)}>Book Now</button>
+            </div>
         </div>
     );
 };
