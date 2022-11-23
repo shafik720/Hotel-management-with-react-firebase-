8da0b719792c53dc5a4afd9cbc1b1b884a671f6e
shafik720 <shafikrasel5@gmail.com>
2022-11-23 22:18:50 +0600

    working on login

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -5,26 +5,30 @@ import './Signup.css'
 const Signup = () => {
     return (
         <div>
-            <div className="container my-3">
+            <div className="container my-5">
                 <div className="row">
                     <div className="col-lg-6 mx-auto">
                         <div className=" form-div ">
-                            <h2>Log In</h2>
+                            <h2 style={{ color: 'red' }}>Sign Up</h2>
                             <hr style={{ marginBottom: "40px" }} />
                             <form action="" >
-                                <div className="email-field">
+                                <div  className="email-field">
                                     <p>Email :</p>
-                                    <input  type="email" name="" id="" />
+                                    <input type="email" name="" id="" />
                                 </div>
-                                <div className="password-field">
+                                <div  className="password-field">
                                     <p>Password :</p>
-                                    <input  type="password" name="" id="" />
+                                    <input type="password" name="" id="" />
+                                </div>
+                                <div  className="password-field">
+                                    <p>Confirm Password :</p>
+                                    <input type="password" name="" id="" />
                                 </div>
                                 <div className="text-center error-area">
                                     
                                 </div>
-                                <button>Login</button>
-                                <p className="signUpText">Dont Have an Account ? <Link to="/signup">Sign Up Here</Link> </p>
+                                <button className="sign-up-button" type='submit'>Sign Up</button>
+                                <p className="signUpText">Already Have an Account ? <Link to="/login">Log in Here</Link> </p>
                                 <h4>Or</h4>
                                 <div draggable className="googleButton">
                                     <img  alt="" />
